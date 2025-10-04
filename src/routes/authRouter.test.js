e6212d42e7304a8345b022c43b27e29ee36f8e93
Amur Bashirov <bashirov@byu.edu>
2025-10-03 19:39:45 -0600

    successfuly created tests for authRouter

---
+++ src/routes/authRouter.test.js
@@ -0,0 +1,54 @@
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+test('logout', async() =>{
+  const newTestUser = { name: 'new pizza diner', email: 'new reg@test.com', password: 'new a' };
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(newTestUser);
+  newTestUserAuthToken = registerRes.body.token;
+  expectValidJwt(newTestUserAuthToken);
+  expect(registerRes.status).toBe(200);
+
+  const logoutRes = await request(app).delete(`/api/auth/`).set('Authorization', `Bearer ${newTestUserAuthToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+
+})
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
+
+test('missing arguments register', async() =>{
+  const newTestUser = { name: 'new pizza diner', email: 'new reg@test.com'};
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(newTestUser);
+  expect(registerRes.status).toBe(400)
+})
+
+test('unauthorized logout without token', async () => {
+  const res = await request(app).delete('/api/auth');
+  expect(res.status).toBe(401);
+  expect(res.body).toHaveProperty('message', 'unauthorized');
+});
