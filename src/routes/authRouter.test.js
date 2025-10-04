64cf5de334dcd0dfab7725f72348be2639b2f72f
Amur Bashirov <bashirov@byu.edu>
2025-10-03 22:53:55 -0600

    fixed it back

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -22,7 +22,19 @@ test('login', async () => {
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
+test('logout', async() =>{
+  const newTestUser = { name: 'new pizza diner', email: 'new reg@test.com', password: 'new a' };
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(newTestUser);
+  newTestUserAuthToken = registerRes.body.token;
+  expectValidJwt(newTestUserAuthToken);
+  expect(registerRes.status).toBe(200);
 
+  const logoutRes = await request(app).delete(`/api/auth/`).set('Authorization', `Bearer ${newTestUserAuthToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+
+})
 
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
